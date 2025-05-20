bd3838b1ab2b5ff2db88ed2e90a4b0d1d79f8dbc
tominmathew <tominmathew@outlook.com>
2025-05-20 14:56:28 +0000

    LeetCode and other sample coding challenges (Using Streams api)

--- src/FirstNonRepeating.java
+++ src/FirstNonRepeating.java
@@ -1,21 +1,37 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.LinkedHashMap;
+import java.util.stream.Collectors;
 
 public class FirstNonRepeating {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String string = "swiss";
+        System.out.println(usingStreams(string));
         System.out.println(firstNonRepeating(string));
     }
-    public static String firstNonRepeating(String string){
+
+    public static String firstNonRepeating(String string) {
         List<Character> set = new ArrayList<>();
-        for(Character c : string.toCharArray()){
-            if(!set.contains(c)){
-               set.add(c);
+        for (Character c : string.toCharArray()) {
+            if (!set.contains(c)) {
+                set.add(c);
             } else {
                 set.remove(c);
             }
         }
 
         return set.getFirst().toString();
+    }
+
+    public static String usingStreams(String string) {
+        return string.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()))
+                .entrySet().stream()
+                .filter(entry -> entry.getValue() == 1)
+                .map(entry -> entry.getKey().toString())
+                .findFirst()
+                .orElse("");
     }
 }
