ec129d857c5feb349bba8b5c5fc08e08d8a211e6
tominmathew <tominmathew@outlook.com>
2025-05-20 14:39:17 +0000

    PatternPrint

--- src/PatternPrint.java
+++ src/PatternPrint.java
@@ -1,26 +1,29 @@
-public class PatternPrint{
+public class PatternPrint {
 
     public static void main(String[] args) {
 
-        System.out.println("patternPrint()");
+        System.out.println(patternPrint());
 
     }
 
-    public static String patternPrint(){
+    public static String patternPrint() {
         StringBuilder sb = new StringBuilder();
         int n = 5;
-        for(int i=0; i<n;i++){
-            for(int j=0;j<n;j++){
-                while (0 < j && j < n){
+
+        for (int i = 0; i < n; i++) {
+            int k = 0;
+            for (int j = 0; j < n; j++) {
+                if (0 < k && k < n-1 && i != 0 && i !=n-1){
                     sb.append(" ");
-                    j++;
-                } else {
+                    k++;
+                } else{
                     sb.append("*");
-                }
+                    k++;
+                    }
             }
             sb.append("\n");
         }
+        return sb.toString();
     }
-    return sb.toString();
-    }
+
 }
