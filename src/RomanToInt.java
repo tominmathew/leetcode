94dbfc55b862f29f3b893069d230d5f7d00ecd51
tominmathew <tomincherukara@gmail.com>
2025-05-22 14:33:21 +0530

    Corrected logic

--- src/RomanToInt.java
+++ src/RomanToInt.java
@@ -6,38 +6,19 @@ public class RomanToInt {
     }
 
     public static int romanToInt(String s) {
-        int[] values = { 1, 5, 10, 50, 100, 500, 1000 };
-        Character[] romans = { 'I', 'V', 'X', 'L', 'C', 'D', 'M' };
+        int[] values = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+        String[] romans = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
 
         int result = 0;
-        int j = 0;
-
-        for (int i = 0; i < romans.length - 1;) {
-            while (s.length() > 1 && j + 1 < s.length()) {
-                if ((s.charAt(j) == 'I') && (s.charAt(j + 1) == 'X')) {
-                    result += 9;
-                } else if ((s.charAt(j) == 'I') && (s.charAt(j + 1) == 'V')) {
-                    result += 5;
-                } else if ((s.charAt(j) == 'X') && (s.charAt(j + 1) == 'C')) {
-                    result += 90;
-                } else if ((s.charAt(j) == 'X') && (s.charAt(j + 1) == 'L')) {
-                    result += 50;
-                } else if ((s.charAt(j) == 'C') && (s.charAt(j + 1) == 'M')) {
-                    result += 900;
-                } else if ((s.charAt(j) == 'C') && (s.charAt(j + 1) == 'D')) {
-                    result += 500;
-                } else {
-                    break;
+        while (s.length() > 0) {
+            for (int j = 0; j < romans.length; j++) {
+                if (s.startsWith(romans[j])) {  
+                    result += values[j];  
+                    s = s.substring(romans[j].length());  
+                    break;  
                 }
             }
-                if (romans[i] == s.charAt(j)) {
-                    result = result + values[i];
-                    s = s.substring(1);
-                    i = 0;
-                } else {
-                    i++;
-                }
-            }
+        }
 
         return result;
 
