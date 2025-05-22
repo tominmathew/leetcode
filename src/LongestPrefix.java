4c4ed7986391982c1b3737ae5376845996664a5d
tominmathew <tomincherukara@gmail.com>
2025-05-22 15:32:36 +0530

    LongestPrefix

---
+++ src/LongestPrefix.java
@@ -0,0 +1,20 @@
+public class LongestPrefix{
+    public static void main(String[] args) {
+        String[] strArray = {"flower","flower","flower"};
+        System.out.println(longestPrefix(strArray));
+    }
+
+    public static String longestPrefix(String[] strs){
+        String prefix = strs[0];
+
+        for(int i = 0; i<strs.length; i++){
+            while(!strs[i].startsWith(prefix)){
+                prefix = prefix.substring(0,prefix.length()-1);
+                if(prefix.isEmpty()){
+                    return "";
+                }
+            }
+        }
+        return prefix;
+    }
+}
